08032a333a3a79336f8cb3ba8c02bcb62d91aee4
Neil Chen <neilchen1998@gmail.com>
2025-09-19 21:25:36 -0500

    reworked on the initialization of confetti and added it in README

---
+++ benchmarks/celebrationbenchmark.cc
@@ -0,0 +1,111 @@
+#include <cstddef>
+#include <vector>   // std::vector
+#include <fstream>  // std::ofstream
+#include <algorithm>    // std::generate
+
+#include "nanobench.h"  // ankerl::nanobench::Bench
+
+#include "raylib.h"
+#include "math/mathlib.hpp"
+#include "gui/colourlib.hpp"
+
+#include "raylib.h"
+
+struct Confetti
+{
+    /// @brief The x and y position of the confetti
+    Vector2 position;
+
+    /// @brief The x and y component of the velocity of the confetti
+    Vector2 velocity;
+
+    /// @brief The physical size in pixel of the confetti 
+    Vector2 size;
+
+    /// @brief The orientation of the confetti
+    float orientation;
+
+    /// @brief The angular velocity of the confetti
+    float omega;
+
+    /// @brief The colour of the confetti
+    Color colour;
+
+    /// @brief True if the confetti is on the screen
+    bool active;
+};
+
+static constexpr std::array<Color, 6> CONFETTI_COLOURS { LIGHT_CORAL, APRICOT, LEMON, MINT, SKY_BLUE, LAVENDER };
+
+static constexpr int NUM_OF_CONFETTI = 2'000;
+
+class Dummy
+{
+public:
+    Dummy() : confetti_(NUM_OF_CONFETTI)
+    {
+
+    }
+
+    void ConstructorA()
+    {
+        auto itr = confetti_.begin();
+        while (itr != confetti_.end())
+        {
+            itr->active = true;
+            itr->position = (Vector2){ (float)GetRandomValue(0, GetScreenWidth()), (float)GetRandomValue(-GetScreenHeight() * 0.25, 0) };
+            itr->velocity = (Vector2){ GetNormalFloatDist(0, 100), GetNormalFloatDist(0, 100) };
+            itr->size = (Vector2){ (float)GetRandomValue(5, 12), (float)GetRandomValue(8, 20) };
+            itr->orientation = (float)GetRandomValue(0, 360);
+            itr->omega = (float)GetNormalFloatDist(-150, 150);
+            itr->colour =  CONFETTI_COLOURS[GetUniformIntDist(0, CONFETTI_COLOURS.size() - 1)];
+
+            ++itr;
+        }
+    }
+
+    void ConstructorB()
+    {
+        std::generate(confetti_.begin(), confetti_.end(), []()
+        {
+            return Confetti
+            {
+                // Use designated initializer
+                .position = { (float)GetRandomValue(0, GetScreenWidth()), (float)GetRandomValue(-GetScreenHeight() * 0.25, 0) },
+                .velocity = { GetNormalFloatDist(0, 100), GetNormalFloatDist(0, 100) },
+                .size = (float)GetRandomValue(5, 12), (float)GetRandomValue(8, 20),
+                .orientation = (float)GetRandomValue(0, 360),
+                .omega = GetNormalFloatDist(-150, 150),
+                .colour = CONFETTI_COLOURS[GetUniformIntDist(0, CONFETTI_COLOURS.size() - 1)],
+                .active = true
+            };
+        });
+    }
+private:
+    std::vector<Confetti> confetti_;
+};
+
+int main()
+{
+    std::ofstream file("./build/benchmarks/celebration-results.csv");
+    ankerl::nanobench::Bench bench;
+
+    bench.minEpochIterations(500).title("Constructors")
+        .run("For Loop", [&]
+        {
+            Dummy d {};
+            d.ConstructorA();
+
+            ankerl::nanobench::doNotOptimizeAway(d);
+        })
+        .run("std::generate", [&]
+        {
+            Dummy d {};
+            d.ConstructorB();
+
+            ankerl::nanobench::doNotOptimizeAway(d);
+        });
+
+    // Render the results to a csv file
+    bench.render(ankerl::nanobench::templates::csv(), file);
+}
--- include/gui/celebrationlib.hpp
+++ include/gui/celebrationlib.hpp
@@ -8,11 +8,20 @@
 #include "math/mathlib.hpp"
 #include "gui/colourlib.hpp"
 
-namespace
+class Celebration
 {
-    constexpr float GRAVITY = 200.0;
-    constexpr size_t MAX_NUM_CONFETTI = 500;
+public:
+    Celebration();
 
+    ~Celebration() = default;
+
+    /// @brief Updates the confetti
+    void Update();
+
+    /// @brief Draws the confetti
+    void Draw() const;
+
+private:
     struct Confetti
     {
         /// @brief The x and y position of the confetti
@@ -31,35 +40,31 @@ namespace
         float omega;
 
         /// @brief The colour of the confetti
-        Color color;
+        Color colour;
 
         /// @brief True if the confetti is on the screen
         bool active;
     };
 
-    std::array<Color, 6> CONFETTI_COLOURS { LIGHT_CORAL, APRICOT, LEMON, MINT, SKY_BLUE, LAVENDER };
-}
-
-class Celebration
-{
-public:
-    Celebration();
-
-    ~Celebration() = default;
-
-    /// @brief Updates the confetti
-    void Update();
-
-    /// @brief Draws the confetti
-    void Draw() const;
-
 private:
-    /// @brief Spawns a single confetti
+    /// @brief Spawns a piece of confetti
     void SpawnConfetti();
 
+    /// @brief Generates a piece of confetti
+    Confetti GenerateConfetti();
+
 private:
-    /// @brief 
+    /// @brief the confetti container
     std::vector<Confetti> confetti_;
+
+    /// @brief The gravity
+    static constexpr float GRAVITY = 200.0;
+
+    /// @brief the maximum pieces of confetti
+    static constexpr std::size_t MAX_NUM_CONFETTI = 400;
+
+    /// @brief the colours of the confetti
+    static constexpr std::array<Color, 6> CONFETTI_COLOURS { LIGHT_CORAL, APRICOT, LEMON, MINT, SKY_BLUE, LAVENDER };
 };
 
 #endif // INCLUDE_GUI_CELEBRATIONLIB_H_
