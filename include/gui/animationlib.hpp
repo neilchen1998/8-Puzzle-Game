04db9b512d1b0d77ace9ecb8720001802134ac31
Neil Chen <neilchen1998@gmail.com>
2025-09-21 14:36:27 -0500

    replaced hardcoded values & changed subtitle

--- include/gui/animationlib.hpp
+++ include/gui/animationlib.hpp
@@ -3,18 +3,6 @@
 
 #include <string>   // std::string
 
-#include "raylib.h"
-
-/// @brief The states of the game
-enum struct LoadingState : unsigned short
-{
-    SMALL_BOX_BLINKING = 0,
-    LEFT_BOX_GROWING,
-    RIGHT_BOX_GROWING,
-    LETTER_APPEARING,
-    ENDING
-};
-
 class RaylibAnimation
 {
 public:
@@ -35,6 +23,17 @@ public:
         return (curState_ == LoadingState::ENDING);
     }
 
+public:
+    /// @brief The states of the game
+    enum struct LoadingState : unsigned short
+    {
+        SMALL_BOX_BLINKING = 0,
+        LEFT_BOX_GROWING,
+        RIGHT_BOX_GROWING,
+        LETTER_APPEARING,
+        ENDING
+    };
+
 private:
     /// @brief The current state of the game
     LoadingState curState_;
@@ -45,27 +44,43 @@ private:
     /// @brief The height of the main screen
     int screenHeight_;
 
+    /// @brief The hight of the left side of the rectangle
     int leftSideRecHeight_;
 
     int topSideRecWidth_;
 
+    /// @brief The hight of the top side of the rectangle
+    int topSideRecHeight_;
+
+    /// @brief The counter that counts the number of letters has appeared so far
     int lettersCount_;
 
+    /// @brief The alpha (saturation) that controls the fading
     float alpha_;
 
+    /// @brief The position x of the logo
     const int logoPositionX_;
 
+    /// @brief The position y of the logo
     const int logoPositionY_;
 
-    /// @brief
+    /// @brief The bottom side rectangle width
     int bottomSideRecWidth_;
 
+    /// @brief The top right side rectangle width
     int rightSideRecHeight_;
 
+    /// @brief The counter that counts the number of frames since animation begins
     unsigned long framesCounter_;
 
-    /// @brief the name of the author
-    std::string author_;
+    /// @brief the width of Raylib
+    int raylibTxtWidth_;
+
+    /// @brief The width of the subtitle
+    int subTxtWidth_;
+
+    /// @brief The subtitle
+    std::string subTitle_;
 };
 
 #endif // INCLUDE_GUI_ANIMATIONLIB_H_
