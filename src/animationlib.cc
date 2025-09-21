bb7a150a65d4973922465aaca5c399a1fb45fb03
Neil Chen <90213006+neilchen1998@users.noreply.github.com>
2025-09-21 14:37:31 -0500

    Introduction Animation Rework (#9)
    * replaced hardcoded values
    * changed subtitle

--- src/animationlib.cc
+++ src/animationlib.cc
@@ -1,22 +1,35 @@
-#include "raylib.h" // GetScreenHeight
+#include "raylib.h" // GetScreenHeight, MeasureText, DrawRectangle, etc.
 
 #include "gui/animationlib.hpp"
+
+namespace
+{
+    constexpr int padding = 10;
+    constexpr int logoWidth = 112;
+    constexpr int authorTxtFont = 50;
+    constexpr int subtxtFont = 30;
+    constexpr int recHeight = 16;
+    constexpr int recWidth = 256;
+}
 
 RaylibAnimation::RaylibAnimation()
     : curState_(LoadingState::SMALL_BOX_BLINKING),
     screenWidth_(GetScreenHeight()),
     screenHeight_(GetScreenHeight()),
-    logoPositionX_(screenWidth_/2 - 128),
-    logoPositionY_(screenHeight_/2 - 128),
-    leftSideRecHeight_(16),
-    topSideRecWidth_(16),
-    bottomSideRecWidth_(16),
-    rightSideRecHeight_(16),
+    logoPositionX_(screenWidth_/2 - recWidth / 2),
+    logoPositionY_(screenHeight_/2 - recWidth / 2),
+    leftSideRecHeight_(recHeight),
+    topSideRecWidth_(recHeight),
+    topSideRecHeight_(recHeight),
+    bottomSideRecWidth_(recHeight),
+    rightSideRecHeight_(recHeight),
     lettersCount_(0),
     framesCounter_(0),
     alpha_(1.0f),
-    author_("Neil")
+    subTitle_ ("made by Neil with blood, sweat, and tears")
 {
+    subTxtWidth_ = MeasureText(subTitle_.data(), subtxtFont);
+    raylibTxtWidth_ = MeasureText("raylib", authorTxtFont);
 }
 
 void RaylibAnimation::Update()
@@ -39,7 +52,7 @@ void RaylibAnimation::Update()
             leftSideRecHeight_ += 4;
 
             // Check the upper left box has completed
-            if (topSideRecWidth_ == 256)
+            if (topSideRecWidth_ == recWidth)
             {
                 curState_ = LoadingState::RIGHT_BOX_GROWING;
             }
@@ -51,7 +64,7 @@ void RaylibAnimation::Update()
             rightSideRecHeight_ += 4;
 
             // Check the bottom right box has completed
-            if (bottomSideRecWidth_ == 256)
+            if (bottomSideRecWidth_ == recWidth)
             {
                 curState_ = LoadingState::LETTER_APPEARING;
             }
@@ -96,44 +109,46 @@ void RaylibAnimation::Draw() const
     {
         case LoadingState::SMALL_BOX_BLINKING:
         {
-            if ((framesCounter_/15)%2) DrawRectangle(logoPositionX_, logoPositionY_, 16, 16, BLACK);
+            if ((framesCounter_/15)%2)
+            {
+                DrawRectangle(logoPositionX_, logoPositionY_, topSideRecHeight_, topSideRecHeight_, BLACK);
+            }
 
             break;
         }
         case LoadingState::LEFT_BOX_GROWING:
         {
-            DrawRectangle(logoPositionX_, logoPositionY_, topSideRecWidth_, 16, BLACK);
-            DrawRectangle(logoPositionX_, logoPositionY_, 16, leftSideRecHeight_, BLACK);
+            DrawRectangle(logoPositionX_, logoPositionY_, topSideRecWidth_, topSideRecHeight_, BLACK);
+            DrawRectangle(logoPositionX_, logoPositionY_, topSideRecHeight_, leftSideRecHeight_, BLACK);
             break;
         }
         case LoadingState::RIGHT_BOX_GROWING:
         {
             DrawRectangle(logoPositionX_, logoPositionY_, topSideRecWidth_, 16, BLACK);
-            DrawRectangle(logoPositionX_, logoPositionY_, 16, leftSideRecHeight_, BLACK);
+            DrawRectangle(logoPositionX_, logoPositionY_, topSideRecHeight_, leftSideRecHeight_, BLACK);
 
-            DrawRectangle(logoPositionX_ + 240, logoPositionY_, 16, rightSideRecHeight_, BLACK);
+            DrawRectangle(logoPositionX_ + 240, logoPositionY_, topSideRecHeight_, rightSideRecHeight_, BLACK);
             DrawRectangle(logoPositionX_, logoPositionY_ + 240, bottomSideRecWidth_, 16, BLACK);
             break;
         }
         case LoadingState::LETTER_APPEARING:
         {
-            DrawRectangle(logoPositionX_, logoPositionY_, topSideRecWidth_, 16, Fade(BLACK, alpha_));
-            DrawRectangle(logoPositionX_, logoPositionY_ + 16, 16, leftSideRecHeight_ - 32, Fade(BLACK, alpha_));
+            DrawRectangle(logoPositionX_, logoPositionY_, topSideRecWidth_, topSideRecHeight_, Fade(BLACK, alpha_));
+            DrawRectangle(logoPositionX_, logoPositionY_ + 16, topSideRecHeight_, leftSideRecHeight_ - 32, Fade(BLACK, alpha_));
 
-            DrawRectangle(logoPositionX_ + 240, logoPositionY_ + 16, 16, rightSideRecHeight_ - 32, Fade(BLACK, alpha_));
+            DrawRectangle(logoPositionX_ + 240, logoPositionY_ + 16, topSideRecHeight_, rightSideRecHeight_ - topSideRecHeight_ * 2, Fade(BLACK, alpha_));
             DrawRectangle(logoPositionX_, logoPositionY_ + 240, bottomSideRecWidth_, 16, Fade(BLACK, alpha_));
 
-            DrawRectangle(GetScreenWidth()/2 - 112, GetScreenHeight()/2 - 112, 224, 224, Fade(RAYWHITE, alpha_));
+            DrawRectangle(GetScreenWidth()/2 - logoWidth, GetScreenHeight()/2 - logoWidth, 224, 224, Fade(RAYWHITE, alpha_));
 
-            DrawText(TextSubtext("raylib", 0, lettersCount_), GetScreenWidth()/2 - 44, GetScreenHeight()/2 + 48, 50, Fade(BLACK, alpha_));
+            DrawText(TextSubtext("raylib", 0, lettersCount_), GetScreenWidth()/2 + logoWidth - raylibTxtWidth_ - padding, GetScreenHeight()/2 + 48, authorTxtFont, Fade(BLACK, alpha_));
 
             // Only show the subtext when the first letter of raylib comes out
-            std::string_view subTitle("made with blood, sweat, and tears");
             if (lettersCount_)
             {
-                const int subTxtWidth = MeasureText(subTitle.data(), 30);
-                DrawText(subTitle.data(), (GetScreenWidth() - subTxtWidth) / 2, GetScreenHeight()/2 + 150, 30, Fade(LIME, alpha_));
+                DrawText(subTitle_.data(), (GetScreenWidth() - subTxtWidth_) / 2, GetScreenHeight()/2 + 150, subtxtFont, Fade(GRAY, alpha_));
             }
+
             break;
         }
         default: break;
